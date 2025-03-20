fff0b20dc570c0216d09b74e36fe791d991417c1
mderlere <cohlias2@gmail.com>
2025-03-20 04:15:59 +0200

    Fixed LoadBanner and finalized ascii-art

--- funcs/loadBanner.go
+++ funcs/loadBanner.go
@@ -10,51 +10,56 @@ import (
 func LoadBanner(font string) (map[rune][]string, error) {
 	filename := "fonts/" + font + ".txt"
 
-	// Check if the file exists before trying to open it
+	// Check if the file exists
 	if _, err := os.Stat(filename); os.IsNotExist(err) {
-		return nil, fmt.Errorf("Font file \"%s\" not found.", font)
+		return nil, fmt.Errorf("font file \"%s\" not found", font)
 	}
 
 	file, err := os.Open(filename)
 	if err != nil {
-		return nil, fmt.Errorf("Could not open banner file \"%s\".", filename)
+		return nil, fmt.Errorf("could not open banner file \"%s\"", filename)
 	}
 	defer file.Close()
 
 	bannerMap := make(map[rune][]string)
 	scanner := bufio.NewScanner(file)
 
-	var currentChar rune = ' ' // Tracks the current character being processed (starting with space)
+	var currentChar rune = ' ' // Tracks the current character being processed
 	var charLines []string
-	linesRead := 0 // Track the number of lines read to detect an empty file
+	linesRead := 0 // Total lines read
 
 	for scanner.Scan() {
-		linesRead++ // Increment counter for each line read
+		linesRead++
 		line := scanner.Text()
 
 		if line == "" {
 			if len(charLines) > 0 {
-				bannerMap[currentChar] = append([]string{}, charLines...) // Store a copy of the character's ASCII representation
+				// Check if the character has exactly 8 lines
+				if len(charLines) != 8 {
+					return nil, fmt.Errorf("banner file \"%s\" is corrupted", filename)
+				}
+				bannerMap[currentChar] = append([]string{}, charLines...)
 				currentChar++
 				charLines = nil
 			}
 			continue
 		}
 
-		charLines = append(charLines, line) // Collect the ASCII art lines for the current character
+		charLines = append(charLines, line)
 	}
 
-	// If no lines were read, return an error
+	// Final check: Last character must also have exactly 8 lines
+	if len(charLines) > 0 && len(charLines) != 8 {
+		return nil, fmt.Errorf("banner file \"%s\" is corrupted", filename)
+	}
+
+	// Check if the file was completely empty
 	if linesRead == 0 {
-		return nil, fmt.Errorf("Banner file \"%s\" is empty.", filename)
-	}
-
-	if len(charLines) > 0 {
-		bannerMap[currentChar] = append([]string{}, charLines...) // Store the last character in the map
+		return nil, fmt.Errorf("banner file \"%s\" is empty", filename)
 	}
 
 	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("Failed to read banner file \"%s\": %s", filename, err)
+		return nil, fmt.Errorf("failed to read banner file \"%s\": %s", filename, err)
 	}
 
 	return bannerMap, nil
